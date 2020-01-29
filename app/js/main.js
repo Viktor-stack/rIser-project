178227f56f8efa5754dfae811ae1df4ad7830612
Viktor-stack <grohik493@gmail.com>
2020-01-29 07:04:43 +0200

    added pagescroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   $(".advantages__tabs-box .tab").on("click", function(event) {
     var id = $(this).attr("data-id");
     $(".advantages__tabs-box")
@@ -14,4 +14,23 @@ $(function () {
       .fadeIn();
     return false;
   });
+  $(window).on("load", function() {
+  
+    $(".nav__list a[href*='#']").mPageScroll2id();
+    $(".header__menu a[href*='#']").mPageScroll2id();
+    
+  });
+
+  // $("#menu, #menu2, #menu3, #menu4,").on("click", "a", function(event) {
+  //   //отменяем стандартную обработку нажатия по ссылке
+  //   event.preventDefault();
+
+  //   //забираем идентификатор бока с атрибута href
+  //   var id = $(this).attr("href"),
+  //     //узнаем высоту от начала страницы до блока на который ссылается якорь
+  //     top = $(id).offset().top;
+
+  //   //анимируем переход на расстояние - top за 1500 мс
+  //   $("header,html").animate({ scrollTop: top }, 1000);
+  // });
 });
